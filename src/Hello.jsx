2b8e051784f970306d3f095b702e94499ba2932f
ishiguro.yo <4243145+yoh1496@users.noreply.github.com>
2022-03-22 22:29:14 +0900

    add initial web app

---
+++ src/Hello.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+const Hello = () => <h1>こんにちは！</h1>;
+export default Hello;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/app.js"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "app.js",
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "node_modules")],
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.jsx$/],
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+  ],
+};
